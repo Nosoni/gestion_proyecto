0a667dc1b77917ffc7e10963a9a0455fc011f7bf
Franco <francisco.recalde.91@gmail.com>
2021-02-24 22:19:01 -0300

    revert

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
-async function license() {
+gulp.task("licenses", async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
@@ -74,7 +74,4 @@ async function license() {
     )
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
-}
-
-gulp.task("licenses", license);
-gulp.task("default", license);
+});
